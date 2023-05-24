cd4f694426d9c4ee576376a1ccff67e62fc7341c
Syric <134160470+CMCanterbury@users.noreply.github.com>
2023-05-24 13:29:15 -0400

    Added Shrimp

--- src/main/java/com/mrmacky/samplecraft/SCItems.java
+++ src/main/java/com/mrmacky/samplecraft/SCItems.java
@@ -1,6 +1,9 @@
 package com.mrmacky.samplecraft;
 
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,6 +21,13 @@ public class SCItems {
     //Registration methods
     static void register(IEventBus bus) {
         ITEMS.register(bus);
+    }
+
+    //Add items to tabs
+    public static void addItemsToTabs(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(new ItemStack(SHRIMP.get()));
+        }
     }
 
 }
